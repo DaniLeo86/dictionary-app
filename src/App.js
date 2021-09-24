babf3ed372a255a2488e4733712922fb5d6adee6
DaniLeo86 <81173570+DaniLeo86@users.noreply.github.com>
2021-09-24 14:51:16 -0400

    Created Dictionary component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
+import React from "react";
 import logo from "./logo.png";
 import './App.css';
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
       <img src={logo} className="App-logo img-fluid" alt="logo" />
       </header>
-    </div>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer"><small>Coded by Danielle Leonard</small></footer>
+    </div></div>
   );
 }
 
