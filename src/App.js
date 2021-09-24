1c1d4609de4c3deecf12804fd5e66c8652ae9cd3
DaniLeo86 <81173570+DaniLeo86@users.noreply.github.com>
2021-09-24 08:39:36 -0400

    Added logo and css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-
+import logo from "./logo.png";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      Hello
+      <img src={logo} className="App-logo img-fluid" alt="logo" />
       </header>
     </div>
   );
