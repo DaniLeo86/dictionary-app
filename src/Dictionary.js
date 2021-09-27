f7d669a0f30385793581401ef5764625288be021
DaniLeo86 <81173570+DaniLeo86@users.noreply.github.com>
2021-09-27 10:18:51 -0400

    Added search engine form

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Dictionary.css";
 
 export default function Dictionary() {
+    let [keyword, setKeyword] = useState("");
+    function search(event) {
+       
+event.preventDefault();
+        alert (`Searching for ${keyword} definition.`);
+    }
+ function handleKeywordChange(event){
 
-    return <div className= "Dictionary">Hello there</div>;
+setKeyword(event.target.value);
+ }
+    return <div className= "Dictionary">
+        <form onSubmit={search}>
+        <input type="search" onChange={handleKeywordChange} />
+        
+        </form></div>;
 }
