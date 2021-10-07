5104a75ec99646f48e266e21bd53d101195ed665
DaniLeo86 <81173570+DaniLeo86@users.noreply.github.com>
2021-10-07 14:42:36 -0400

    Added photo api

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -5,7 +5,7 @@ export default function Phonetic(props) {
   return (
     <div className="Phonetic">
       <span className="text"> {props.phonetic.text} </span>{" "}
-      <a href={props.phonetic.audio} target="_blank">
+      <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
         Listen
       </a>
     </div>
