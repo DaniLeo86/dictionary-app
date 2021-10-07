26d12e464dc5b9c393870cbdbadfe2e318986c5c
DaniLeo86 <81173570+DaniLeo86@users.noreply.github.com>
2021-10-07 11:19:13 -0400

    Added default keyword to load

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React from "react";
 import logo from "./logo.png";
-import './App.css';
+import "./App.css";
 import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-      <header className="App-header">
-      <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
-      <main>
-        <Dictionary />
-      </main>
-      <footer className="App-footer"><small>Coded by Danielle Leonard</small></footer>
-    </div></div>
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary defaultKeyword="Global" />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Danielle Leonard</small>
+        </footer>
+      </div>
+    </div>
   );
 }
 
